1d5674771cebeba6f7f6cb867e3a731db22535d2
Jacob Hardman <42230736+thejacobhardman@users.noreply.github.com>
2019-11-05 12:43:58 -0700

    Add project files.

---
+++ Lottery-Application/Lottery-Application.cpp
@@ -0,0 +1,16 @@
+// Jacob Hardman
+// Comp Sci 1
+// Professor Jennifer Bailey
+// 11/5/19
+
+#include <iostream>
+#include <iomanip>
+#include <random>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
