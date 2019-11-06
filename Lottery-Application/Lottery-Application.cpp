009dfd7f5181ae63ed8fb0bca385dc814b0481ea
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2019-11-05 19:27:49 -0700

    finish core functions and extra credit features

--- Lottery-Application/Lottery-Application.cpp
+++ Lottery-Application/Lottery-Application.cpp
@@ -7,35 +7,104 @@
 #include <iomanip>
 #include <random>
 #include <time.h>
-#include <math.h>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int* generateLotto() {
+int* generateLotto(int& powerball) {
 	static int num[5];
-
-	srand((unsigned)time(NULL));
+	vector<int> alreadyGenerated;
 
 	for (int i = 0; i < 5; i++) {
+		bool allUnique = false;
 		num[i] = rand() % 9 + 0;
+
+		if (alreadyGenerated.size() > 0) {
+			while (!allUnique) {
+				if (find(alreadyGenerated.begin(), alreadyGenerated.end(), num[i]) != alreadyGenerated.end()) {
+					num[i] = rand() % 9 + 0;
+				}
+				else {
+					allUnique = true;
+					alreadyGenerated.push_back(num[i]);
+				}
+			}
+		}
+		else {
+			alreadyGenerated.push_back(num[i]);
+		}
 	}
+
+	powerball = rand() % 3 + 1;
 
 	return num;
 }
 
-int* getPlayerGuess() {
+int* getPlayerGuess(int& playerPowerball) {
 	static int num[5];
+	vector<int> alreadyGuessed;
+	int size = sizeof(alreadyGuessed);
 
 	for (int i = 0; i < 5; i++) {
+		bool allUnique = false;
 		cout << "Number " << i + 1 << ": ";
 		cin >> num[i];
+		if (alreadyGuessed.size() > 0) {
+			while (!allUnique) {
+				if (find(alreadyGuessed.begin(), alreadyGuessed.end(), num[i]) != alreadyGuessed.end()) {
+					cout << "You've already guessed that number. Please enter a unique number." << endl;
+					cout << "Number " << i + 1 << ": ";
+					cin >> num[i];
+				}
+				else {
+					allUnique = true;
+					alreadyGuessed.push_back(num[i]);
+				}
+			}
+		}
+		else {
+			alreadyGuessed.push_back(num[i]);
+		}
 	}
+
+	cout << "Enter your guess for the powerball: ";
+	cin >> playerPowerball;
 
 	return num;
 }
 
-int calculateResults(int *lotto, int *playerGuess, double& playerWinnings, double& playerBalance) {
-	static int matches = 0;
+int* getPlayerGuess(bool isRandom, int& playerPowerball) {
+	static int num[5];
+	vector<int> alreadyGenerated;
+
+	for (int i = 0; i < 5; i++) {
+		bool allUnique = false;
+		num[i] = rand() % 9 + 0;
+
+		if (alreadyGenerated.size() > 0) {
+			while (!allUnique) {
+				if (find(alreadyGenerated.begin(), alreadyGenerated.end(), num[i]) != alreadyGenerated.end()) {
+					num[i] = rand() % 9 + 0;
+				}
+				else {
+					allUnique = true;
+					alreadyGenerated.push_back(num[i]);
+				}
+			}
+		}
+		else {
+			alreadyGenerated.push_back(num[i]);
+		}
+	}
+
+	playerPowerball = rand() % 3 + 1;
+
+	return num;
+}
+
+int calculateResults(int *lotto, int *playerGuess, double& winAmount, double& playerWinnings, double& playerBalance, int& powerball, int& playerPowerball) {
+	int matches = 0;
 
 	for (int i = 0; i < 5; i++) {
 		for (int j = 0; j < 5; j++) {
@@ -50,57 +119,60 @@ int calculateResults(int *lotto, int *playerGuess, double& playerWinnings, doubl
 	switch (matches)
 	{
 	case 1:
-		playerWinnings = 1.00;
-		playerBalance += playerWinnings;
+		winAmount = 1.00;
 		break;
 	case 2:
-		playerWinnings = 2.00;
-		playerBalance += playerWinnings;
+		winAmount = 2.00;
 		break;
 	case 3:
-		playerWinnings = 5.00;
-		playerBalance += playerWinnings;
+		winAmount = 5.00;
 		break;
 	case 4:
-		playerWinnings = 10.00;
-		playerBalance += playerWinnings;
+		winAmount = 10.00;
 		break;
 	case 5:
-		playerWinnings = 20.00;
-		playerBalance += playerWinnings;
-		break;
-	default:
-		playerWinnings = 0.00;
+		winAmount = 20.00;
 		break;
 	}
+
+	if (powerball == playerPowerball) {
+		winAmount *= 2.00;
+	}
+
+	playerBalance += winAmount;
+	playerWinnings += winAmount;
 
 	return matches;
 }
 
-void displayResults(int* lotto, int* playerGuess, int& matchedNums, double& playerWinnings) {
+void displayResults(int* lotto, int* playerGuess, int& powerball, int& playerPowerball, int& matchedNums, double& winAmount) {
 
-	cout << "Lottery Numbers: " << endl;
+	cout << "Lottery Numbers: ";
 	for (int i = 0; i < 5; i++) {
 		cout << lotto[i] << " ";
 	}
 
-	cout << endl;
+	cout << "Powerball: " << powerball << endl;
 
-	cout << "Your Numbers: " << endl;
+	cout << "Your Numbers:    ";
 	for (int i = 0; i < 5; i++) {
 		cout << playerGuess[i] << " ";
 	}
 
+	cout << "Powerball: " << playerPowerball << endl;
+
 	cout << endl << "You matched " << matchedNums << " numbers." << endl;
-	if (playerWinnings > 0.00) {
-		cout << "You won $" << playerWinnings << "!!!" << endl;
+	if (winAmount > 0.00) {
+		cout << "You won $" << winAmount << "!!!" << endl;
 	}
 	else {
 		cout << "You didn't win anything." << endl;
 	}
+
+	if (powerball == playerPowerball) { cout << "You matched the powerball! Winnings were increased by 2x!" << endl; }
 }
 
-void playGame(bool& isRunning, double& playerBalance) {
+void playGame(bool& isRunning, double& playerBalance, double& playerWinnings) {
 	bool userConfirm = false;
 	string userInput;
 	while (userConfirm == false) {
@@ -110,6 +182,7 @@ void playGame(bool& isRunning, double& playerBalance) {
 		if (userInput == "Y" || userInput == "y") {
 			userConfirm = true;
 			playerBalance -= 5.00;
+			playerWinnings -= 5.00;
 			cout << endl;
 		}
 		else if (userInput == "N" || userInput == "n") {
@@ -125,39 +198,77 @@ void playGame(bool& isRunning, double& playerBalance) {
 int main()
 {
 	//Init Start Params
-	double playerBalance = 10.00, playerWinnings = 0.00;
-	int *lotto, *playerGuess;
+	double playerBalance = 10.00, playerWinnings = 0.00, winAmount = 0.00;
+	int* lotto, * playerGuess = 0, powerball = 0, playerPowerball = 1;
+	string userInput;
 
 	cout << fixed << setprecision(2);
+	srand((unsigned)time(NULL));
+
 	cout << "Welcome to Lotto Winners!" << endl;
 
-	bool isRunning = true, didPlayerWin;
+	bool isRunning = true;
 	do {
-		lotto = generateLotto();
+		lotto = generateLotto(powerball);
 
 		//Checking if the player wants to play again.
 		cout << endl << "You currently have $" << playerBalance << endl << "It costs $5.00 to play the game." << endl;
-		playGame(isRunning, playerBalance);
+		playGame(isRunning, playerBalance, playerWinnings);
 		if (isRunning == false) { break; }
 
-		//Getting the player's guess
-		cout << "Enter your 5 lottery number picks: " << endl;
-		playerGuess = getPlayerGuess();
+		//Is the player's guess random?
+		bool isRandom = false, userConfirm = false;
+		while (userConfirm == false) {
+			cout << "Would you like to generate your guess randomly? (Y/N): ";
+			cin >> userInput;
 
-		int matchedNums = calculateResults(lotto, playerGuess, playerWinnings, playerBalance);
+			if (userInput == "Y" || userInput == "y") {
+				isRandom = true;
+				userConfirm = true;
+				playerGuess = getPlayerGuess(isRandom, playerPowerball); //Generating the player's guess
+			}
+			else if (userInput == "N" || userInput == "n") {
+				userConfirm = true;
+				cout << "Enter your 5 lottery number picks: " << endl;
+				playerGuess = getPlayerGuess(playerPowerball); //Getting the player's guess
+			}
+			else {
+				cout << "Please enter a valid selection." << endl;
+			}
+		}
 		
-		displayResults(lotto, playerGuess, matchedNums, playerWinnings);
+		//Calculating the results
+		int matchedNums = calculateResults(lotto, playerGuess, winAmount, playerWinnings, playerBalance, powerball, playerPowerball);
+		
+		//Displaying the results
+		displayResults(lotto, playerGuess, powerball, playerPowerball, matchedNums, winAmount);
 
-	} while (isRunning&& playerBalance >= 0.00);
+		//Debug
+
+	} while (isRunning&& playerBalance >= 5.00);
 
 	//Display results
- 	if (playerWinnings > 0.00) {
-		cout << endl << "You won $" << playerBalance - 10.00 << "!" << endl << "Congratulations!" << endl;
+	//The player lost the game.
+	if (playerBalance < 5.00) { 
+		cout << endl << "You only have $" << playerBalance << ". That's not enough to play the game." << endl; 
+		if (playerBalance == 0.00) {
+			cout << endl << "You lost all of your money!" << endl << "Better luck next time!" << endl;
+		}
+		else {
+			cout << endl << "You lost $" << (playerBalance - 10.00) * -1 << endl << "Better luck next time!" << endl;
+		}
 	}
-	else if (playerWinnings < 0.00) {
-		cout << endl << "You lost all of your money!" << endl << "Better luck next time!" << endl;
-	}
+	//The player chose to quit
 	else {
-		cout << endl << "You broke even." << endl << "Nice try!" << endl;
+		if (playerBalance > 10.00 && playerWinnings > 0.00) {
+			cout << endl << "You won $" << playerBalance - 10.00 << "!" << endl << "Congratulations!" << endl;
+		}
+		else if (playerWinnings < 0.00 && playerBalance > 0.00) {
+			cout << endl << "You lost $" << (playerBalance - 10.00) * -1 << endl << "Better luck next time!" << endl;
+		}
+		else if (playerWinnings == 0.00)
+		{
+			cout << endl << "You broke even." << endl << "Nice try!" << endl;
+		}
 	}
 }
